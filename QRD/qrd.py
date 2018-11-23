a1bd95b7c64d261335f3e87eecefe656070b65ad
Raphael Mitsch <laarem33@hotmail.com>
2018-11-23 16:49:31 +0100

    Added logging.

--- QRD/qrd.py
+++ QRD/qrd.py
@@ -1,16 +1,19 @@
 """
 Implementation of Householder-reflection based QR decomposition of nxm matrices.
 """
+import logging
 import math
 import argparse
 import numpy as np
+from typing import Union
 
 
-def householder(alpha, x):
+def householder(alpha, x, logger: logging.Logger) -> Union[np.ndarray, int]:
     """
     Computes Householder vector for alpha and x.
     :param alpha:
     :param x:
+    :param logger:
     :return:
     """
 
@@ -29,14 +32,42 @@ def householder(alpha, x):
     return v, tau
 
 
+def qr_decomposition(A: np.ndarray, m: int, n: int, logger: logging.Logger) -> np.ndarray:
+    """
+    Applies Householder-based QR decomposition on specified matrix A.
+    :param A:
+    :param m:
+    :param n:
+    :param logger:
+    :return:
+    """
+    logger.info("Starting QR decomposition.")
+
+    for j in range(0, n):
+        print(j)
+
+    # todo
+    #   - go through individual vectors, apply householder transformation.
+    #   - piece together Q^T, R, Q. compare with correct result.
+
+    return A
+
+
 if __name__ == '__main__':
     # todo
     #   generate nxm matrix
+    #   timing
     #   call householder() with corresponding vectors.
+
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s : %(levelname)s : %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    logger = logging.getLogger("log")
 
     parser = argparse.ArgumentParser(description='QR decomposition based on Householder reflections.')
     parser.add_argument('-m', '--m', dest='m', type=int, help='m for matrix A of size mxn.', required=True)
     parser.add_argument('-n', '--n', dest='n', type=int, help='n for matrix A of size mxn.', required=True)
     args = vars(parser.parse_args())
+    m = args["m"]
+    n = args["n"]
 
-    print(args["n"])
+    A = qr_decomposition(np.random.rand(m, n), m, n, logger)
+
