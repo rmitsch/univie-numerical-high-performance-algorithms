QRD: Fixed bug in non-updating computation.

QRD/qrd.py
@@ -2,83 +2,73 @@
 Implementation of Householder-reflection based QR decomposition of nxm matrices.
 """
 import logging
-import math
+
 import argparse
 import numpy as np
 from typing import Union
 
 
-def householder(alpha: float, x: np.ndarray) -> Union[np.ndarray, int]:
+def householder(x: np.ndarray) -> Union[np.ndarray, int]:
     """
-    Computes Householder vector for alpha and x.
-    :param alpha:
-    :param x:
+    Computes v and tau for Householder transformation of x.
+    Note that
+    https://stackoverflow.com/questions/53489237/qr-decomposition-with-householder-transformations-introduction-of-zeroes-uncle
+    helped in getting the implementation right.
+    :param x: Vector.
     :return:
     """
 
-    s = math.pow(np.linalg.norm(x, ord=2), 2)
-    v = x
+    # Use vectorized approach as shown in https://rosettacode.org/wiki/QR_decomposition#Python.
+    v = x / (x[0] + np.copysign(np.linalg.norm(x), x[0]))
+    v[0] = 1
+    tau = 2 / (v.T @ v)
 
-    if s == 0:
-        tau = 0
-    else:
-        t = math.sqrt(alpha * alpha + s)
-        v_one = alpha - t if alpha <= 0 else -s / (alpha + t)
-
-        tau = 2 * v_one * v_one / (s + v_one * v_one)
-        v /= v_one
+    # Down here: Approach as in paper with slight modifications.
+    # alpha = x[0]
+    # s = np.power(np.linalg.norm(x[1:]), 2)
+    # v = x.copy()
+    #
+    # if s == 0:
+    #     tau = 0
+    # else:
+    #     t = np.sqrt(alpha**2 + s)
+    #     v[0] = alpha - t if alpha <= 0 else -s / (alpha + t)
+    #
+    #     tau = 2 * v[0]**2 / (s + v[0]**2)
+    #     v /= v[0]
 
     return v, tau
 
 
-def qr_decomposition(A: np.ndarray, m: int, n: int, logger: logging.Logger) -> Union[np.ndarray, np.ndarray]:
+def qr_decomposition(A: np.ndarray, logger: logging.Logger) -> Union[np.ndarray, np.ndarray]:
     """
-    Applies Householder-based QR decomposition on specified matrix A.
+    Decomposes rectangular matrix A in matrices Q and R.
+    Note that
+    https://stackoverflow.com/questions/53489237/qr-decomposition-with-householder-transformations-introduction-of-zeroes-uncle
+    helped in getting the implementation right.
     :param A:
-    :param m:
-    :param n:
     :param logger:
     :return:
     """
     logger.info("Starting QR decomposition.")
-    H = []
-    R = A
-    Q = A
-    I = np.eye(m, m)
+
+    m, n = A.shape
+    R = A.copy()
+    Q = np.identity(m)
 
     for j in range(0, n):
         # Apply Householder transformation.
-        x = A[j + 1:m, j]
-        v_householder, tau = householder(np.linalg.norm(x), x)
-        v = np.zeros((1, m))
-        v[0, j] = 1
-        v[0, j + 1:m] = v_householder
+        v, tau = householder(R[j:, j])
+        H = np.identity(m)
+        H[j:, j:] -= tau * v.reshape(-1, 1) * v
+        R = H @ R
+        Q = H @ Q
 
-        # Note that this sequence of steps is not the most efficient one - see slides or
-        # http://home.ku.edu.tr/~emengi/teaching/math504_f2011/Lecture12_new.pdf: Multiplying in the right sequence with
-        # A directly is more performant.
-        res = I - tau * v * np.transpose(v)
-        # if j > 0:
-            # for k in range(0, j):
-            #     res[k, k] = 1
-
-        R = np.matmul(res, R)
-        H.append(res)
-
-    R = A
-    for h in H:
-        print(h)
-        R = np.matmul(h, R)
-
-    return Q, R
+    # Note that triu is used here mainly for cosmetic reasons - we discard of the entries under the diagonale.
+    return Q[:n].T, np.triu(R[:n])
 
 
 if __name__ == '__main__':
-    # todo
-    #   generate nxm matrix
-    #   timing
-    #   call householder() with corresponding vectors.
-
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s : %(levelname)s : %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
     logger = logging.getLogger("log")
 
@@ -91,7 +81,7 @@ if __name__ == '__main__':
 
     A = np.random.rand(m, n)
     q, r = np.linalg.qr(A)
-    Q, R = qr_decomposition(A, m, n, logger)
+    Q, R = qr_decomposition(A, logger)
 
     print("*****")
     print(Q)